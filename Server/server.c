591fa7ecaa28506c887e8e6fcd1dcee5a3c3ef85
Luca-Bianco <luca.bianco4@studenti.unina.it>
2022-04-27 20:08:26 +0200

    dichiarate prime variabili e incluse le prime librerie

--- Server/server.c
+++ Server/server.c
@@ -1,6 +1,20 @@
 /*Server*/
+#include<sys/socket.h>
+#include<stdio.h>
+#include<sys/un.h>
+#include<unistd.h>
+#include<signal.h>
 
 int main() {
- 
+
+  int server_socket_descriptor, client_socket_descriptor;
+  int server_len, client_len;
+  struct sockaddr_un server_addr;
+  struct sockaddr_un client_addr;
+
+  /* TODO generale:
+  set server_addr.sun_family and sun_path, socket(), bind(), listen(), while cicle*/
+  
+
  return 0;
 }
